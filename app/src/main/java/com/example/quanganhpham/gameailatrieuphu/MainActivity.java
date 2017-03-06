00ca40276e59183663de7a03cb1fac900e363cd1
Quang Anh <quanganhpham300596@gmail.com>
2017-03-07 00:20:41 +0700

    Animation commit

--- app/src/main/java/com/example/quanganhpham/gameailatrieuphu/MainActivity.java
+++ app/src/main/java/com/example/quanganhpham/gameailatrieuphu/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.quanganhpham.gameailatrieuphu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_start;
+    Button button_exit;
+    boolean clicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        button_start = (Button) findViewById(R.id.button_start);
+        button_exit = (Button) findViewById(R.id.button_exit);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!clicked) {
+                    button_start.animate().translationX(400).withLayer();
+                    clicked = true;
+                } else {
+                    button_start.animate().translationX(0).withLayer();
+                    clicked = false;
+                }
+            }
+        });
+        button_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!clicked) {
+                    button_exit.animate().translationY(50).withLayer();
+                    clicked = true;
+                } else {
+                    button_exit.animate().translationY(0).withLayer();
+                    clicked = false;
+                }
+
+            }
+        });
     }
 }
