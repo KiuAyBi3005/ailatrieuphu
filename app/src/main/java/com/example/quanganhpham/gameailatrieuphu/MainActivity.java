22dafa889f72832979065466be88d20b6859d5f1
Quang Anh <quanganhpham300596@gmail.com>
2017-03-17 10:37:00 +0700

    Update mainactivity

--- app/src/main/java/com/example/quanganhpham/gameailatrieuphu/MainActivity.java
+++ app/src/main/java/com/example/quanganhpham/gameailatrieuphu/MainActivity.java
@@ -3,60 +3,70 @@ package com.example.quanganhpham.gameailatrieuphu;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonStart;
-    Button buttonExit;
-    ImageView imageGame;
-    Context context = this;
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-        buttonStart = (Button) findViewById(R.id.button_start);
-        buttonExit  = (Button) findViewById(R.id.button_exit);
-        buttonStart.setTextColor(Color.CYAN);
-        buttonExit.setTextColor(Color.CYAN);
-        buttonStart.setOnClickListener(click_start);
-        buttonExit.setOnClickListener(click_exit);
+        findViewById(R.id.button_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.button_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("Warning");
+                builder.setMessage("Do you want to exit the app?");
+                builder.setCancelable(false);
+                builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
     }
 
-    public View.OnClickListener click_start = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(getApplicationContext(), PlayActivity.class);
-            startActivity(intent);
-        }
-    };
-    public View.OnClickListener click_exit = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-            alertDialogBuilder.setMessage("Are you sure you want to exit ?").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    System.exit(0);
-                }
-            })
-                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.cancel();
-                        }
-                    });
-            AlertDialog alertDialog = alertDialogBuilder.create();
-        }
-    };
+
+    //public View.OnClickListener click_exit = new View.OnClickListener() {
+    //    @Override
+    //    public void onClick(View view) {
+    //        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+    //        alertDialogBuilder.setMessage("Are you sure you want to exit ?").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+    //            @Override
+    //            public void onClick(DialogInterface dialogInterface, int i) {
+    //                System.exit(0);
+    //            }
+    //        })
+    //                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+    //                    @Override
+    //                    public void onClick(DialogInterface dialogInterface, int i) {
+    //                        dialogInterface.cancel();
+    //                    }
+    //                });
+    //        AlertDialog alertDialog = alertDialogBuilder.create();
+    //    }
+    //};
 }
 
