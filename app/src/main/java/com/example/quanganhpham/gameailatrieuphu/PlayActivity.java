584e81c75ba74bec018a2364a10cc0bca733c9e9
Quang Anh <quanganhpham300596@gmail.com>
2017-03-17 08:35:52 +0700

    Update

--- app/src/main/java/com/example/quanganhpham/gameailatrieuphu/PlayActivity.java
+++ app/src/main/java/com/example/quanganhpham/gameailatrieuphu/PlayActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -17,88 +18,56 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 /**
  * Created by QuangAnhPham on 3/15/2017.
  */
 
 public class PlayActivity extends AppCompatActivity {
-    String answer = "";
-    Context context;
-    int number_question = 15;
-    int index = 0;
-    List<Question> list_question;
-    Question now_question;
-    int true_answer = 0;
-    int content = 0;
-    int money = 0;
-    int miliSecond = 31000;
+    Context mContext;
+    int NUMBER_QUESTION = 15;
+    int mIndex = 0;
+    int trueAnswer = 0;
+    int mContent = 0;
+    int mMoney = 0;
+    int MILISECOND = 31000;
+    List<Question> listQuestion;
+    Question nowQuestion;
     CountDownTimer countTime, depay;
-    TextView text_question, text_notification, text_time, text_money;
-    RadioButton radio_answer_a, radio_answer_b, radio_answer_c, radio_answer_d;
-    RadioGroup radioGroup;
-    Button button_confirm, button_check;
-    CheckBox checkbox_5050, checkbox_ask_audience, checkbox_call, checkbox_nextquestion;
+    TextView textTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        checkbox_5050 = (CheckBox) findViewById(R.id.checkbox_5050);
-        checkbox_nextquestion = (CheckBox) findViewById(R.id.checkbox_nextquestion);
-        checkbox_call = (CheckBox) findViewById(R.id.checkbox_call);
-        checkbox_ask_audience = (CheckBox) findViewById(R.id.checkbox_ask_audience);
-        text_question = (TextView) findViewById(R.id.text_question);
-        text_money = (TextView) findViewById(R.id.text_money);
-        text_time = (TextView) findViewById(R.id.text_time);
-        text_notification = (TextView) findViewById(R.id.text_notification);
-        radio_answer_a = (RadioButton) findViewById(R.id.radio_answer_a);
-        radio_answer_a = (RadioButton) findViewById(R.id.radio_answer_a);
-        radio_answer_c = (RadioButton) findViewById(R.id.radio_answer_c);
-        radio_answer_d = (RadioButton) findViewById(R.id.radio_answer_d);
-        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
-        button_confirm = (Button) findViewById(R.id.button_confirm);
-        button_check = (Button) findViewById(R.id.button_check);
-        text_notification.setTextColor(Color.YELLOW);
-        text_question.setTextColor(Color.YELLOW);
-        text_time.setTextColor(Color.YELLOW);
-        text_money.setTextColor(Color.YELLOW);
-        radio_answer_a.setTextColor(Color.WHITE);
-        radio_answer_b.setTextColor(Color.WHITE);
-        radio_answer_c.setTextColor(Color.WHITE);
-        radio_answer_d.setTextColor(Color.WHITE);
-        button_check.setTextColor(View.GONE);
-        Database db = new Database(this);
+        Database database = new Database(this);
         try {
-            db.createDatabase();
+            database.createDatabase();
         } catch (IOException e) {
             e.printStackTrace();
             Toast.makeText(this, "Erorr", Toast.LENGTH_SHORT).show();
         }
-        list_question = new ArrayList<Question>();
-        list_question = db.randomQuestion(number_question);
-
-
+        listQuestion = new ArrayList<Question>();
+        listQuestion = database.randomQuestion(NUMBER_QUESTION);
     }
 
     public void time() {
-        countTime = new CountDownTimer(miliSecond, 1000) {
+        countTime = new CountDownTimer(MILISECOND, 1000) {
             @Override
             public void onTick(long l) {
-                text_time.setText("Time" + l / 1000);
+                textTime.setText("Time" + l / 1000);
             }
 
             @Override
             public void onFinish() {
                 checkCorrect();
-                index++;
-                if (index < number_question) {
-                    show(index);
+                mIndex++;
+                if (mIndex < NUMBER_QUESTION) {
+                    show(mIndex);
                     time();
                 } else {
-                    index = 0;
-                    button_confirm.setVisibility(View.GONE);
-                    button_check.setVisibility(View.VISIBLE);
-                    text_time.setVisibility(View.GONE);
+                    mIndex = 0;
                     countTime.cancel();
                 }
             }
@@ -107,108 +76,78 @@ public class PlayActivity extends AppCompatActivity {
     }
 
     public void show(int cuscor) {
-        radio_answer_a.setTextColor(Color.WHITE);
-        radio_answer_b.setTextColor(Color.WHITE);
-        radio_answer_c.setTextColor(Color.WHITE);
-        radio_answer_d.setTextColor(Color.WHITE);
-        text_notification.setText("Sentence :" + (cuscor + 1) + "/" + (number_question));
-        now_question = list_question.get(cuscor);
-        text_money.setText(now_question.mQuestion);
-        text_money.setText("You got :" + money + " USD");
-        radio_answer_a.setText(now_question.mAnswer_a);
-        radio_answer_b.setText(now_question.mAnswer_b);
-        radio_answer_c.setText(now_question.mAnswer_c);
-        radio_answer_d.setText(now_question.mAnswer_d);
-        radioGroup.clearCheck();
+        nowQuestion = listQuestion.get(cuscor);
     }
 
     public void checkCorrect() {
         String answer = "";
-        if (radio_answer_a.isChecked() == true) {
-            answer = "a";
-        } else if (radio_answer_b.isChecked() == true) {
-            answer = "b";
-        } else if (radio_answer_c.isChecked() == true) {
-            answer = "c";
-        } else if (radio_answer_d.isChecked() == true) {
-            answer = "d";
-        }
-        list_question.get(index).mAnswer_players = answer;
-        if (answer.equalsIgnoreCase(now_question.mAnswers_questions)) {
-            true_answer += 1;
-            switch (index) {
+
+        listQuestion.get(mIndex).mAnswerPlayers = answer;
+        if (answer.equalsIgnoreCase(nowQuestion.mAnswersQuestions)) {
+            trueAnswer += 1;
+            switch (mIndex) {
                 case 0:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 1:
-                    money = 1000;
+                    mMoney = 1000;
                     break;
                 case 2:
-                    money = 100;
+                    mMoney = 100;
                     break;
                 case 3:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 4:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 5:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 6:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 7:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 8:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 9:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 10:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 11:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 12:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 13:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 14:
-                    money = 500;
+                    mMoney = 500;
                     break;
                 case 15:
-                    money = 500;
+                    mMoney = 500;
                     break;
             }
-            text_money.setText("You got :" + money + "  USD");
-            text_money.setTextColor(Color.YELLOW);
         } else {
-            if (index < 4) {
-                money = 0;
+            if (mIndex < 4) {
+                mMoney = 0;
             }
-            if (index < 9 && index >= 4) {
-                money = 88888;
+            if (mIndex < 9 && mIndex >= 4) {
+                mMoney = 88888;
             }
         }
-        if (index < 14 && index >= 9) {
-            money = 1000000;
-            text_money.setTextColor(Color.YELLOW);
-            text_money.setText("You got :" + money + "  USD");
-            index = number_question;
-            if (index >= number_question) {
-                text_notification.setText("Total :");
-                text_question.setText("Your right choose" + true_answer + "sentences");
-                radio_answer_a.setVisibility(View.GONE);
-                radio_answer_b.setVisibility(View.GONE);
-                radio_answer_c.setVisibility(View.GONE);
-                radio_answer_d.setVisibility(View.GONE);
+        if (mIndex < 14 && mIndex >= 9) {
+            mMoney = 1000000;
 
+            mIndex = NUMBER_QUESTION;
+            if (mIndex >= NUMBER_QUESTION) {
             }
         }
     }
